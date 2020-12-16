bc838b8f79d991f7a84e94f6f3c9d5d74fa33adf
Neil Flood <neilflood@fastmail.fm>
2020-12-16 17:06:24 +1000

    Added spectral cluster function

--- pyshepseg.py
+++ pyshepseg.py
@@ -14,3 +14,51 @@ numba compiled code for the other main components.
 # Just in case anyone is trying to use this with Python-2
 from __future__ import print_function, division
 
+import numpy
+from sklearn import KMeans
+from numba import jit
+
+
+def doShepherdSegmentation(img, numClusters=60, clusterSubsamplePcnt=1):
+    """
+    Perform Shepherd segmentation in memory, on the given 
+    multi-band img array.
+    
+    The img array has shape (nBands, nRows, nCols).
+    
+    """
+    clusters = makeSpectralClusters(img, 
+        numClusters=numClusters,
+        subsamplePcnt=clusterSubsamplePcnt)
+    
+
+
+
+def makeSpectralClusters(img, numClusters=60, subsamplePcnt=1):
+    """
+    First step of Shepherd segmentation. Use K-means clustering
+    to create a set of "seed" segments, labelled only with
+    their spectral cluster number. 
+    
+    The img array has shape (nBands, nRows, nCols).
+
+    """
+    (nBands, nRows, nCols) = img.shape
+
+    # Re-organise the image data so it matches what sklearn
+    # expects.
+    xFull = numpy.transpose(img, axes=(1, 2, 0))
+    xFull = xFull.reshape((nRows*nCols, nBands))
+
+    skip = int(round(100./subsamplePcnt))
+    xSample = xFull[::skip]
+
+    km = KMeans(n_clusters=numClusters)
+    km.fit(xSample)
+
+    clustersFull = km.predict(xFull)
+    del xFull, xSample
+    clustersImg = clustersFull.reshape((nRows, nCols))
+
+    return clustersImg
+
