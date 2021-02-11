1a4edb734bc5a36dcc382bf2303cb31208b034b0
Neil Flood <neilflood@fastmail.fm>
2021-02-11 19:29:28 +1000

    Package-level docstring now mentions the tiling module

--- pyshepseg/__init__.py
+++ pyshepseg/__init__.py
@@ -21,7 +21,11 @@
 
 """
 Python implementation of image segmentation algorithm by 
-Shepherd et al (2019). See pyshepseg.shepseg module for details. 
+Shepherd et al (2019). 
+
+The main algorithm is implemented in the pyshepseg.shepseg module. 
+For memory-efficient tiled segmentation of larger rasters,
+see the pyshepseg.tiling module. 
 
 """
 
