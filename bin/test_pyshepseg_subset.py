53510343b5ac879366997a8ba66dc840da2c5c9b
Sam Gillingham <gillingham.sam@gmail.com>
2021-09-30 13:26:13 +1000

    added --srcwin and --projwin options

--- bin/test_pyshepseg_subset.py
+++ bin/test_pyshepseg_subset.py
@@ -27,7 +27,10 @@ Test harness for subsetting a segmented image
 
 from __future__ import print_function, division
 
+import math
 import argparse
+from osgeo import gdal
+gdal.UseExceptions()
 from pyshepseg import tiling
 
 DFLT_OUTPUT_DRIVER = 'KEA'
@@ -42,14 +45,13 @@ def getCmdargs():
         help="Input file")
     p.add_argument("-o", "--outfile", required=True,
         help="Output file")
-    p.add_argument("--tlx", type=int, required=True,
-        help="Top left x (in pixel coords) of the --infile to start extracting")
-    p.add_argument("--tly", type=int,  required=True,
-        help="Top left y (in pixel coords) of the --infile to start extracting")
-    p.add_argument("--xsize", type=int,  required=True,
-        help="X size of the window to extract")
-    p.add_argument("--ysize", type=int,  required=True,
-        help="Y size of the window to extract")
+    group = p.add_mutually_exclusive_group(required=True)
+    group.add_argument("--srcwin", type=int, nargs=4,
+        metavar=('xoff', 'yoff', 'xsize', 'ysize'),
+        help="Top left pixel coordinates and subset size (in pixels) to extract")
+    group.add_argument("--projwin", type=float, nargs=4,
+        metavar=('ulx', 'uly', 'lrx', 'lry'),
+        help="Projected coordinates to extract subset from the input file")
     p.add_argument("--origsegidcol", help="Name of column to write the original" +
                 " segment ids")
     p.add_argument("-f", "--format", default=DFLT_OUTPUT_DRIVER, 
@@ -57,16 +59,51 @@ def getCmdargs():
         help="Name of output GDAL format that supports RATs (default=%(default)s)")
     cmdargs = p.parse_args()
     return cmdargs
+    
+def getPixelCoords(fname, coords):
+    """
+    Open the supplied file and work out what coords (ulx, uly, lrx, lry)
+    are in pixel coordinates and return (tlx, tly, xsize, ysize)
+    """
+    ulx, uly, lrx, lry = coords
+    ds = gdal.Open(fname)
+    transform = ds.GetGeoTransform()
+    invTransform = gdal.InvGeoTransform(transform)
+    
+    pix_tlx, pix_tly = gdal.ApplyGeoTransform(invTransform, ulx, uly)
+    pix_brx, pix_bry = gdal.ApplyGeoTransform(invTransform, lrx, lry)
+    pix_tlx = int(pix_tlx)
+    pix_tly = int(pix_tly)
+    pix_brx = int(math.ceil(pix_brx))
+    pix_bry = int(math.ceil(pix_bry))
+    
+    if (pix_tlx < 0 or pix_tly < 0 or pix_brx >= ds.RasterXSize or 
+            pix_bry >= ds.RasterYSize):
+        msg = 'Specified coordinates not completely within image'
+        raise ValueError(msg)
+        
+    xsize = pix_brx - pix_tlx
+    ysize = pix_bry - pix_tly
+    return pix_tlx, pix_tly, xsize, ysize
 
 def main():
     cmdargs = getCmdargs()
+    
+    tlx = None
+    tly = None
+    xsize = None
+    ysize = None
+    if cmdargs.srcwin is not None:
+        tlx, tly, xsize, ysize = cmdargs.srcwin
+    elif cmdargs.projwin is not None:
+        tlx, tly, xsize, ysize = getPixelCoords(cmdargs.infile, cmdargs.projwin)
     
     creationOptions = []
     if cmdargs.format in GDAL_DRIVER_CREATION_OPTIONS:
         creationOptions = GDAL_DRIVER_CREATION_OPTIONS[cmdargs.format]
     
-    tiling.subsetImage(cmdargs.infile, cmdargs.outfile, cmdargs.tlx, cmdargs.tly, 
-        cmdargs.xsize, cmdargs.ysize, cmdargs.format, creationOptions, 
+    tiling.subsetImage(cmdargs.infile, cmdargs.outfile, tlx, tly, 
+        xsize, ysize, cmdargs.format, creationOptions, 
         cmdargs.origsegidcol)
     
 if __name__ == "__main__":
