9f432d18d788a13b5636435afeb158e3d431e8b8
Sam Gillingham <gillingham.sam@gmail.com>
2021-12-10 09:01:43 +1000

    use numpy.distutils as distutils now deprecated (#22)

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@
 #CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
 #WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from distutils.core import setup
+from numpy.distutils.core import setup
 import pyshepseg
 
 setup(name='pyshepseg',
