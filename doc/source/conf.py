61c9dd95ca0eac6c356da6f8457ec9fa00bd9a05
Neil Flood <neilflood@fastmail.fm>
2022-11-23 07:16:16 +1000

    set numpydoc_show_class_members to True, to avoid the extra auto-generated table of methods for each class, and avoid warnings from Sphinx about the difficulty it has generating this

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,6 +31,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'numpydoc'
 ]
+numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
