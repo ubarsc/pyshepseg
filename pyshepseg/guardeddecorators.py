2caead8b8eb46dc4547ea178e77e42b6a61cdcbf
Neil Flood <neilflood@fastmail.fm>
2022-11-29 10:36:19 +1000

    Remove a couple of trailing spaces

--- pyshepseg/guardeddecorators.py
+++ pyshepseg/guardeddecorators.py
@@ -3,7 +3,7 @@ A dreadful hack to get around the fact that the numpydoc Sphinx extension
 does not play well with numba's jitclass decorator.
 
 If we are running with Sphinx, then fake the jitclass decorator so that it
-just returns the class it is decorating. 
+just returns the class it is decorating.
 
 """
 import sys
@@ -14,7 +14,7 @@ else:
     def jitclass(cls_or_spec=None, spec=None):
         """
         Our fake jitclass decorator. Hacked from the real one
-        in numba. 
+        in numba.
 
         Returns
         -------
